7b8e2d8abaef2b8825804c52cf85ff45fa9b0bf7
tuphs28 <28tbush@gmail.com>
2026-02-09 20:28:31 +0000

    lecture04

--- lecture04/simplenet.py
+++ lecture04/simplenet.py
@@ -38,27 +38,285 @@ def main(
     key = jax.random.key(seed=seed)
     
     print("load the training data...")
-    # TODO
+    with jnp.load("../data/mnist.npz") as datafile:
+        x_train = jnp.array(datafile["x_train"])
+        x_test = jnp.array(datafile["x_test"])
+        y_train = jnp.array(datafile["y_train"])
+        y_test = jnp.array(datafile["y_test"])
+
+    print(x_train.shape, x_train.dtype)
+    print(x_test.shape, x_test.dtype)
+    print(y_train.shape, y_train.dtype)
+    print(y_test.shape, y_test.dtype)
+    
+    print(mp.image(x_train[0]))
+    
+    x_train = 1.275 * x_train / 255 - 0.1
+    x_test = 1.275 * x_test / 255 - 0.1
 
     print("initialising the model...")
-    # TODO
+    key_model_init, key = jax.random.split(key)
+    model = SimpLeNet.init(key=key_model_init)
+    print(jax.tree.map(lambda leaf: leaf.shape, model))
+    
+    num_params = sum([leaf.size for leaf in jax.tree.leaves(model)])
+    print(f"{num_params=}")
 
     print("training...")
-    # TODO
     
+    losses = []
+    accuracies = []
+    for step in range(num_steps):
+        # sample a minibatch
+        key_batch, key = jax.random.split(key)
+        batch_idxs = jax.random.choice(
+                key=key_batch,
+                a=y_train.size,
+                shape=(batch_size,),
+                replace=False
+            )
+        x_batch = x_train[batch_idxs]
+        y_batch = y_train[batch_idxs]
+        
+        # compute batch loss and grads
+        loss, grads = jax.value_and_grad(batch_cross_entropy)(model, x_batch, y_batch)
+
+        # apply grads to model for gd
+        model = jax.tree.map(
+                lambda w, g: w - learning_rate * g,
+                model,
+                grads
+            )
+
+        # track metrics 
+        losses.append((step, loss))
+        test_acc = batch_accuracy(model, x_test[:1000], y_test[:1000])
+        accuracies.append((step, test_acc))
+
+        # vis
+        digit_plot = vis_digits(
+                digits=x_test[jnp.array([1,6,7,8])],
+                labels=y_test[jnp.array([1,6,7,8])],
+                model=model
+            )
+        metrics_plot = vis_metrics(
+                losses=losses,
+                accuracies=accuracies,
+                total_num_steps=num_steps
+            )
+
+        plot = digit_plot / metrics_plot
+
+        if step == 0:
+            print(plot)
+        else:
+            print(f"{-plot}{plot}")
 
 # # # 
 # Architecture
 
+def scaled_tanh(x):
+    return 1.7159 * jnp.tanh(0.6667 * x)
 
+
+@jax.tree_util.register_dataclass
+@dataclasses.dataclass
+class AffineTransform:
+    weights: Float[Array, "n_in n_out"]
+    biases: Float[Array, "n_out"]
+
+    def forward(
+            self: Self,
+            x: Float[Array, "n_in"]
+            ) -> Float[Array, "n_out"]:
+        return x @ self.weights + self.biases
+
+    @staticmethod
+    def init(
+            key: PRNGKeyArray,
+            num_inputs: int,
+            num_outputs: int
+            ) -> Self:
+        bound = 1 / jnp.sqrt(num_inputs)
+        return AffineTransform(
+                weights=jax.random.uniform(
+                    key=key,
+                    shape=(num_inputs, num_outputs),
+                    minval=-bound,
+                    maxval=bound
+                    ),
+                biases=jnp.zeros(num_outputs)
+            )
+
+
+@jax.tree_util.register_dataclass        
+@dataclasses.dataclass
+class Subsample2x2:
+    weights: Float[Array, "c 1 1"]
+    biases: Float[Array, "c 1 1"]
+
+    def forward(
+            self: Self,
+            x: Float[Array, "c 2h 2w"]
+        ) -> Float[Array, "c h w"]:
+
+        sums = einops.reduce(
+                x,
+                "c (h 2) (w 2) -> c h w",
+                "sum"
+                )
+
+        return sums * self.weights + self.biases
+
+    @staticmethod
+    def init(num_channels) -> Self:
+        return Subsample2x2(
+                weights=jnp.ones(shape=(num_channels, 1, 1)) / 4, 
+                biases=jnp.zeros(shape=(num_channels, 1, 1))
+                )
+
+
+@functools.partial(
+        jax.tree_util.register_dataclass,
+        data_fields=["kernel"],
+        meta_fields=["padding"]
+)
+@dataclasses.dataclass
+class SimpleConv2D:
+    kernel: Float[Array, "k k c_in c_out"]
+    padding: int
+
+    def forward(
+            self: Self,
+            x: Float[Array, "c_in h w"]
+        ) -> Float[Array, "c_out h_ w_"]:
+        x_1chw = einops.rearrange(x, "c h w -> 1 c h w")
+        y_1chw = jax.lax.conv_general_dilated(
+                 lhs=x_1chw,
+                 rhs=self.kernel,
+                 window_strides=(1,1),
+                 padding=[(self.padding, self.padding), (self.padding, self.padding)],
+                 dimension_numbers=("NCHW", "HWIO", "NCHW"),
+                 )
+        y_chw = y_1chw[0]
+        return y_chw
+
+    @staticmethod
+    def init(
+            key: PRNGKeyArray,
+            kernel_size: int,
+            num_channels_in: int,
+            num_channels_out: int,
+            padding: int
+        ) -> Self:
+        bound = 1 / jnp.sqrt(num_channels_in*kernel_size**2)
+        return SimpleConv2D(
+                padding=padding,
+                kernel=jax.random.uniform(
+                    key=key,
+                    shape=(
+                        kernel_size,
+                        kernel_size,
+                        num_channels_in,
+                        num_channels_out
+                        ),
+                    minval=-bound,
+                    maxval=bound
+                    )
+                )
+
+
+@jax.tree_util.register_dataclass
+@dataclasses.dataclass
 class SimpLeNet:
-    """TODO"""
+    C1: SimpleConv2D
+    S2: Subsample2x2
+    C3: SimpleConv2D
+    S4: Subsample2x2
+    C5: SimpleConv2D
+    F6: AffineTransform
+    Out: AffineTransform
+
+    def forward(
+            self: Self,
+            image: Float[Array, "28 28"]
+            ) -> Float[Array, "10"]:
+        x = einops.rearrange(image, "h w -> 1 h w") # -> 1x28x28
+        x = scaled_tanh(self.C1.forward(x)) # -> 6x28x28
+        x = scaled_tanh(self.S2.forward(x)) # -> 6x14x14
+        x = scaled_tanh(self.C3.forward(x)) # -> 16x10x10
+        x = scaled_tanh(self.S4.forward(x)) # -> 16x5x5
+        x = scaled_tanh(self.C5.forward(x)) # -> 120x1x1
+        x = jnp.ravel(x) # -> 120
+        x = scaled_tanh(self.F6.forward(x)) # -> 84
+        x = self.Out.forward(x) # -> 10
+        return jax.nn.softmax(x)
+
+    def batch_forward(
+            self: Self,
+            images: Float[Array, "batch_Size 28 28"],
+        ) -> Float[Array, "batch_size 10"]:
+
+        vmapped_forward = jax.vmap(self.forward)
+        return vmapped_forward(images)
+
+    @staticmethod
+    def init(
+            key: PRNGKeyArray,
+        ) -> Self:
+        k1, k2, k3, k4, k5 = jax.random.split(key, 5)
+        return SimpLeNet(
+            C1=SimpleConv2D.init(key=k1, kernel_size=5, num_channels_in=1, num_channels_out=6, padding=2),
+            S2=Subsample2x2.init(num_channels=6),
+            C3=SimpleConv2D.init(key=k2, kernel_size=5, num_channels_in=6, num_channels_out=16, padding=0),
+            S4=Subsample2x2.init(num_channels=16),
+            C5=SimpleConv2D.init(key=k3, kernel_size=5, num_channels_in=16, num_channels_out=128, padding=0),
+            F6=AffineTransform.init(key=k4, num_inputs=128, num_outputs=84),
+            Out=AffineTransform.init(key=k5, num_inputs=84, num_outputs=10)
+                )
 
 
 # # # 
 # Metrics
 
-# TODO
+def cross_entropy(
+        model: SimpLeNet,
+        x: Float[Array, "h w"],
+        y: int
+    ) -> float:
+    probs = model.forward(x)
+    return - jnp.log(probs[y])
+
+
+def batch_cross_entropy(
+        model: SimpLeNet,
+        xs: Float[Array, "batch_size h w"],
+        ys: Int[Array, "batch_size"]
+    ) -> float:
+    
+    vmapped_xent = jax.vmap(
+            cross_entropy,
+            in_axes=(None, 0, 0)
+    )
+
+    all_xents = vmapped_xent(
+            model,
+            xs,
+            ys
+        )
+
+    return jnp.mean(all_xents)
+
+
+def batch_accuracy(
+        model: SimpLeNet,
+        xs: Float[Array, "batch_size h w"],
+        ys: Int[Array, "batch_size"]
+    ) -> float:
+    pred_prob_all_classes = model.batch_forward(xs)
+    highest_prob_class = pred_prob_all_classes.argmax(axis=1)
+    correct_classifications = (ys == highest_prob_class)
+    return jnp.mean(correct_classifications)
 
 
 # # # 
